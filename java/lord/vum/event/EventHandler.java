d375e79a4a7831e40acb2c14d4dfcb7628668c45
Unknown <anon@mail.com>
2018-09-22 22:27:22 -0400

    Make dams not look like a WIP mess

--- java/lord/vum/event/EventHandler.java
+++ java/lord/vum/event/EventHandler.java
@@ -3,7 +3,6 @@ package lord.vum.event;
 import java.util.Random;
 
 import lord.vum.init.BlockInit;
-import lord.vum.util.GeneratorUtils;
 import lord.vum.world.feature.WorldGenBeaverDam;
 import lord.vum.world.feature.WorldGenMuddyLake;
 import net.minecraft.block.state.IBlockState;
@@ -11,19 +10,21 @@ import net.minecraft.init.Biomes;
 import net.minecraft.init.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import net.minecraft.world.gen.feature.WorldGenBlockBlob;
+import net.minecraft.world.biome.Biome;
 import net.minecraft.world.gen.feature.WorldGenSand;
+import net.minecraftforge.common.BiomeDictionary;
 import net.minecraftforge.event.terraingen.DecorateBiomeEvent;
 import net.minecraftforge.event.terraingen.PopulateChunkEvent;
+import net.minecraftforge.event.terraingen.PopulateChunkEvent.Populate;
 import net.minecraftforge.fml.common.eventhandler.Event.Result;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.event.terraingen.PopulateChunkEvent.Populate;
 
 public class EventHandler {
 	private static final IBlockState WATER = Blocks.WATER.getDefaultState();
 	private static final int RIVER_MUD_PER_CHUNK = 2;
 	private static final WorldGenSand MUD_FEATURE = new WorldGenSand(BlockInit.MUD, 4);
-	private static final WorldGenBeaverDam DAM_FEATURE = new WorldGenBeaverDam();
+	private static final WorldGenBeaverDam DAM_FEATURE = new WorldGenBeaverDam(BlockInit.STICK_BLOCK.getDefaultState(), BlockInit.MUD.getDefaultState());
+	private static final int DAM_FREQUENCY = 3; //lower numbers = more dams. set this to 0 to crash
 	public enum Direction { NE, E, SE, NW, W, SW };
 	
 	//Replace vanilla lakes with our lakes that contains mud blocks around the edges
@@ -54,83 +55,90 @@ public class EventHandler {
 			}
 		}
 		
-		for(int m=0; m<1; m++) {
-			int x = rand.nextInt(16) + 8;
-	        int z = rand.nextInt(16) + 8;
+		if(rand.nextInt(DAM_FREQUENCY) == 0) {
+			//smaller rng range for location to prevent cascading... hopefully 
+			int x = rand.nextInt(8) + 12;
+	        int z = rand.nextInt(8) + 12;
 	        BlockPos pos = new BlockPos(event.getChunkPos().getXStart() + x, world.getSeaLevel()-1, event.getChunkPos().getZStart() + z);
 			if(world.getBiome(pos) == Biomes.RIVER) {
 				IBlockState state = world.getBlockState(pos);
-				if(state != WATER)
-					break;
-				
-				Direction firstBank = null;
-				BlockPos firstBankPos = null;
-				
-				//check east, north east, south east (in that order) from starting location to find first river bank
-				for(int riverSearch=1; riverSearch<10; riverSearch++) {
-					//check east block
-					state = world.getBlockState(getDirectionalPos(pos, Direction.E, riverSearch));
-					if(state != WATER) {
-						firstBank = Direction.E;
-						firstBankPos = getDirectionalPos(pos, Direction.E, riverSearch);
-						break;
-					}
+				if(state == WATER)
+				{
+					Direction firstBankDir = null;
+					BlockPos firstBankPos = null;
 					
-					//check north east block
-					state = world.getBlockState(getDirectionalPos(pos, Direction.NE, riverSearch));
-					if(state != WATER) {
-						firstBank = Direction.NE;
-						firstBankPos = getDirectionalPos(pos, Direction.NE, riverSearch);
-						break;
-					}
-					
-					//check south east block
-					state = world.getBlockState(getDirectionalPos(pos, Direction.SE, riverSearch));
-					if(state != WATER) {
-						firstBank = Direction.SE;
-						firstBankPos = getDirectionalPos(pos, Direction.SE, riverSearch);
-						break;
-					}
-				}
-				
-				//make sure we actually found a bank before continuing 
-				if(firstBank != null) {
-					world.setBlockState(pos.add(0,2,0), Blocks.WOOL.getDefaultState()); //TODO remove
-					Direction secondBank = null;
-					BlockPos secondBankPos = null;
-					Direction dirToTest = null;
-					
-					switch(firstBank) {
-						case NE:
-							dirToTest = Direction.SW;
-							break;
-						case E:
-							dirToTest = Direction.W;
-							break;
-						case SE:
-							dirToTest = Direction.NW;
-							break;
-						default:
-							//how did you get here
-							break;
-					}
-					
-					for(int riverSearch=1; riverSearch<7; riverSearch++) {
-						state = world.getBlockState(getDirectionalPos(pos, dirToTest, riverSearch));
+					//check east, north east, south east (in that order) from starting location to find first river bank
+					for(int riverSearch=1; riverSearch<10; riverSearch++) {
+						//check east block
+						state = world.getBlockState(getDirectionalPos(pos, Direction.E, riverSearch));
 						if(state != WATER) {
-							secondBank = Direction.SE;
-							secondBankPos = getDirectionalPos(pos, dirToTest, riverSearch);
+							firstBankDir = Direction.E;
+							firstBankPos = getDirectionalPos(pos, Direction.E, riverSearch);
+							break;
+						}
+						
+						//check north east block
+						state = world.getBlockState(getDirectionalPos(pos, Direction.NE, riverSearch));
+						if(state != WATER) {
+							firstBankDir = Direction.NE;
+							firstBankPos = getDirectionalPos(pos, Direction.NE, riverSearch);
+							break;
+						}
+						
+						//check south east block
+						state = world.getBlockState(getDirectionalPos(pos, Direction.SE, riverSearch));
+						if(state != WATER) {
+							firstBankDir = Direction.SE;
+							firstBankPos = getDirectionalPos(pos, Direction.SE, riverSearch);
 							break;
 						}
 					}
 					
-					if(secondBank != null) {
-						//ayyy we found two sides to a river
-//						for (BlockPos pixel : GeneratorUtils.getBresehnamArrays(firstBankPos.add(0,1,0), secondBankPos.add(0,1,0))) {
-//							world.setBlockState(pixel, Blocks.NETHER_BRICK.getDefaultState());
-//						}
-						DAM_FEATURE.generate(world, rand, firstBankPos, secondBankPos, firstBank);
-						world.setBlockState(pos.add(0,2,0), Blocks.NETHER_BRICK.getDefaultState());
+					//make sure we actually found a bank before continuing 
+					if(firstBankDir != null) {
+						Direction secondBankDir = null;
+						BlockPos secondBankPos = null;
+						Direction dirToTest = null;
+						
+						switch(firstBankDir) {
+							case NE:
+								dirToTest = Direction.SW;
+								break;
+							case E:
+								dirToTest = Direction.W;
+								break;
+							case SE:
+								dirToTest = Direction.NW;
+								break;
+							default:
+								//how did you get here
+								break;
+						}
+						
+						for(int riverSearch=1; riverSearch<7; riverSearch++) {
+							state = world.getBlockState(getDirectionalPos(pos, dirToTest, riverSearch));
+							if(state != WATER) {
+								secondBankDir = Direction.SE;
+								secondBankPos = getDirectionalPos(pos, dirToTest, riverSearch);
+								break;
+							}
+						}
+						
+						if(secondBankDir != null) {
+							
+							//check that at least one edge of the dam is touching a forest type biome
+							Biome firstBankBiome = world.getBiome(getDirectionalPos(firstBankPos, firstBankDir, 4));
+							Biome secondBankBiome = world.getBiome(getDirectionalPos(secondBankPos, secondBankDir, 4));
+							if(BiomeDictionary.hasType(firstBankBiome, BiomeDictionary.Type.FOREST) || 
+							   BiomeDictionary.hasType(secondBankBiome, BiomeDictionary.Type.FOREST)) {
+								
+								DAM_FEATURE.generate(world, rand, firstBankPos, secondBankPos, firstBankDir);
+								//world.setBlockState(pos.add(0,2,0), Blocks.NETHER_BRICK.getDefaultState());//TODO remove
+							}
+								
+								
+	
+						}
 					}
 				}
 			}
--- java/lord/vum/world/feature/WorldGenBeaverDam.java
+++ java/lord/vum/world/feature/WorldGenBeaverDam.java
@@ -2,35 +2,67 @@ package lord.vum.world.feature;
 
 import java.util.Random;
 
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.gen.feature.WorldGenerator;
 import lord.vum.event.EventHandler.Direction;
+import lord.vum.util.GeneratorUtils;
 
-public class WorldGenBeaverDam {
+public class WorldGenBeaverDam { //TODO c beavers
+	private final IBlockState blockyMain;
+	private final IBlockState blockySecond;
+	private final static IBlockState AIR = Blocks.AIR.getDefaultState();
 	
-	public WorldGenBeaverDam() {
-		
+	public WorldGenBeaverDam(IBlockState blocky, IBlockState blocky2) {
+		this.blockyMain = blocky;
+		this.blockySecond = blocky2;
 	}
 	
 	public boolean generate(World worldIn, Random rand, BlockPos start, BlockPos end, Direction dir) {
+		
+		//get the length of dam about to generate
+		double a = Math.abs(start.getX() - end.getX());
+		double b = Math.abs(start.getZ() - end.getZ());
+		double damLength = Math.sqrt(a*a + b*b);
+		if(damLength < 5.0)
+			return false; //no shorties
+		
 		short[][][] damMap = genDamMap(dir, rand);
+		BlockPos center = new BlockPos((start.getX()+end.getX())/2, (start.getY()+end.getY())/2, (start.getZ()+end.getZ())/2);
+		int riverDepth = worldIn.getSeaLevel() + 1 - GeneratorUtils.getOceanSurfaceHeight(worldIn, center.getX(), center.getZ());
+		int damHeight = riverDepth + rand.nextInt(2) + 3;
+		
+		//make sure the dam hight doesn't go out of bounds of array
+		damHeight = damHeight > 11? 11: damHeight;
+		
+		//make sure damn height doesn't build higher than sealevel +2
+		damHeight = damHeight+center.getY()-riverDepth > worldIn.getSeaLevel()+2? worldIn.getSeaLevel()+2-(center.getY()-riverDepth): damHeight;
 		
 		//do one pass of the mock game of life to smooth out rough edges
 		damMap[0] = cellStep(damMap);
 		
 		//make 3 more layers above base
-		for(int n=1; n<=3; n++) {
+		for(int n=1; n<=damHeight; n++) {
 			damMap[n] = layerStep(damMap, rand, n);
 		}
+		damMap = hollowDam(damMap, damHeight);
 		
-		BlockPos center = new BlockPos((start.getX()+end.getX())/2, (start.getY()+end.getY())/2, (start.getZ()+end.getZ())/2);
 		for(int x=0; x<25; x++) {
 			for(int z=0; z<25; z++) {
-				for(int y=0; y<=3; y++) {
-					if(damMap[y][x][z] == 1) {
-						worldIn.setBlockState(center.add(x-12, y, z-12), Blocks.DIAMOND_BLOCK.getDefaultState()); //TODO actually check what block we're overriding
+				for(int y=0; y<=damHeight; y++) {
+					if((center.getY()+y-riverDepth)<worldIn.getSeaLevel()+2)
+					{
+						if(damMap[y][x][z] == 1) {//TODO actually check what block we're overriding
+							if(rand.nextInt(100) > 20)
+								worldIn.setBlockState(center.add(x-12, y-riverDepth, z-12), blockyMain);
+							else
+								worldIn.setBlockState(center.add(x-12, y-riverDepth, z-12), blockySecond);
+						}
+						else if(damMap[y][x][z] == 2) {
+							worldIn.setBlockState(center.add(x-12, y-riverDepth, z-12), AIR); 
+						}
 					}
 				}
 			}
@@ -39,8 +71,8 @@ public class WorldGenBeaverDam {
 	}
 	
 	//makes the base map with '1' values in a rectangle with rough edges facing the direction of dir
-	private short[][][] genDamMap(Direction dir, Random rand) { //TODO add dist
-		short[][][] damMap = new short[10][25][25];
+	private short[][][] genDamMap(Direction dir, Random rand) { 
+		short[][][] damMap = new short[12][25][25];
 		
 		if(dir == Direction.E) {
 			for(int z=9; z<16; z++) {
@@ -62,6 +94,30 @@ public class WorldGenBeaverDam {
 						damMap[0][x][z-x+12] = 1;
 					else if(dir == Direction.SE)
 						damMap[0][x][z+x-12] = 1;
+				}
+			}
+		}
+		
+		//try to add some more randomness to the shape
+		for(int bNum=0; bNum<3; bNum++) {
+			int blobXOffset = 9;
+			int blobZOffset = 10;
+			int addBlobX = rand.nextInt(9)+blobXOffset;
+			int addBlobZ = rand.nextInt(6)+blobZOffset;
+			int blobSize = rand.nextInt(3)+2;
+			for(int xdif=-blobSize; xdif<=blobSize; xdif++) {
+				for(int zdif=-blobSize; zdif<=blobSize; zdif++) {
+					if(xdif*xdif+zdif*zdif <= blobSize*blobSize) {
+					
+						if(rand.nextInt(100) > 20) {
+							if(dir == Direction.E)
+								damMap[0][addBlobX+xdif][addBlobZ+zdif] = 1;
+							else if(dir == Direction.NE)
+								damMap[0][addBlobX+xdif][addBlobZ+zdif-(addBlobX+xdif)+12] = 1;
+							else if(dir == Direction.SE)
+								damMap[0][addBlobX+xdif][addBlobZ+zdif+(addBlobX+xdif)-12] = 1;
+						}
+					}					
 				}
 			}
 		}
@@ -117,6 +173,33 @@ public class WorldGenBeaverDam {
 			}
 		}
 		
+		return outMap;
+	}
+	
+	//turn blocks to '2' values (air) if they have solid blocks on all 6 faces
+	private short[][][] hollowDam(short[][][] inMap, int height) {
+		short[][][] outMap = new short[height][25][25];
+		for(int x=0; x<25; x++) {
+			for(int z=0; z<25; z++) {
+				for(int y=0; y<height; y++) {
+					if(x>0 && y>0 && z>0 &&
+					   x<24 && z<24 && y<height-1 &&
+					   inMap[y][x][z] == 1 && 
+					   inMap[y-1][x][z] == 1 &&
+					   inMap[y+1][x][z] == 1 &&
+					   inMap[y][x-1][z] == 1 &&
+					   inMap[y][x+1][z] == 1 &&
+					   inMap[y][x][z-1] == 1 &&
+					   inMap[y][x][z+1] == 1) {
+						outMap[y][x][z] = 2;
+					}
+					else {
+						outMap[y][x][z] = inMap[y][x][z];
+					}
+						
+				}
+			}
+		}
 		return outMap;
 	}
 }
